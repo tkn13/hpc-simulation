b758948be0e40b5ef90c69893ec023a5626f86e0
tkn13 <thaksin.ckn@gmail.com>
2025-05-23 22:52:53 +0700

    master delet unused code

--- src/master.cpp
+++ src/master.cpp
@@ -31,17 +31,3 @@ void Master::operator()()
     }
     XBT_INFO("master end");
 }
-
-void Master::ping()
-{
-    
-    int size = hostList.size();
-    for (int i = 0; i < size; i++) {
-        int* payload = new int(1);
-        outBox = simgrid::s4u::Engine::get_instance()->mailbox_by_name_or_create(hostList[i]->get_cname());
-        XBT_INFO("[sending...]send data message to mailbox: %s", hostList[i]->get_cname());
-        outBox->put(payload, sizeof(int));
-        XBT_INFO("[sent]send data message to mailbox: %s", hostList[i]->get_cname());
-
-    }
-}
